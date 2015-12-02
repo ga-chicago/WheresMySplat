09a37ac1c8d190d4c8e3fd512827107ee5dba886
James Haff <jehaff@James-Haffs-MacBook-Pro.local>
2015-12-01 20:44:52 -0600

    itemsController

--- controllers/ItemsController.rb
+++ controllers/ItemsController.rb
@@ -1,33 +1,57 @@
 class ItemsController < ApplicationController
 
+  get '/splat' do
 
-get '/splat' do
+      erb :Splat_index
 
-    erb :Splat
+  end
+
+  get '/splat/create' do
+
+    @items = ItemsModel.new
+
+    @items.name = params[:name]
+
+    erb :students_create
+
+  end
+
+  get '/splat/edit/:id' do
+
+    @id = params[id]
+    @items = ItemModel.find(@id)
+
+    erb :splat_edit
+  end
+
+  post '/splat/create' do
+
+    @items = ItemsModel.new
+
+    @items.name = params[:name]
+    @items.quantity = params[:quantity]
+
+    erb :splat_create_success
+  end
+
+  post '/splat/edit' do
+
+    @items = ItemsModel.find(params[:id])
+    @items.name = params[:name]
+    @items.quantity = params[:quantity]
+    @items.save
+
+    erb :splat_edit_success
+
+  end
+
+  post '/delete' do
+
+    @id = params[:id]
+    @items = ItemsModel.find(@id)
+    @items.destroy
+
+    erb :splat_delete_success
+  end
 
 end
-
-get '/splat/create' do
-
-  @items = ItemsModel.new
-
-  @items.name = params[:name]
-
-end
-
-get '/splat/edit/:id' do
-
-  @id = params[id]
-  @items = ItemModel.find(@id)
-
-end
-
-post '/splat/create' do
-
-  @items = ItemsModel.new
-
-  @items.name = params[:name]
-
-end
-
-post '/splat/edit' do 
