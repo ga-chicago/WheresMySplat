4e6b1f60d111e85d36430b5991976a32afe0646e
James Traver <jtraverjr@gmail.com>
2015-12-01 20:41:24 -0600

    updated gulp and package.json

--- js/transpiled/app.js
+++ js/transpiled/app.js
@@ -2,6 +2,9 @@
 var app = app || {};
 window.onload = function() {
 
+  // comment out for prod
+  console.log('application loaded');
+
   var Model = (function(){"use strict";var PRS$0 = (function(o,t){o["__proto__"]={"a":t};return o["a"]===t})({},{});var DP$0 = Object.defineProperty;var GOPD$0 = Object.getOwnPropertyDescriptor;var MIXIN$0 = function(t,s){for(var p in s){if(s.hasOwnProperty(p)){DP$0(t,p,GOPD$0(s,p));}}return t};var proto$0={};
     function Model(properties) {
       this.properties = properties;
