163569a7130d076f7b3a25204fbb94dd1ab3a5f5
James Haff <jehaff@James-Haffs-MacBook-Pro.local>
2015-12-01 20:27:00 -0600

    applicationController

---
+++ controllers/ApplicationController.rb
@@ -0,0 +1,17 @@
+class ApplicationController < Sinatra::Base
+
+  require 'bundler'
+  Bundler.require()
+
+  ActiveRecord::Base.establish_connection(
+    :adapter => 'postgresql',
+    :database => 'where_is_my_splat'
+  )
+
+set :views, File.expand_path('../..views', __FILE__)
+set :views, File.expand_path('../..public', __FILE__)
+
+
+not_found do
+  erb :not_found
+end
---
+++ controllers/ItemsController.rb
@@ -0,0 +1,17 @@
+class ItemsController < ApplicationController
+
+
+get '/splat' do
+
+    erb :Splat
+
+end
+
+get '/splat/create' do
+
+  @items = ItemsModel.new
+
+  @items
+
+
+end
